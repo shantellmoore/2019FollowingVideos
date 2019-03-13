39535ae0d1212286f4b196f41cfdf1f38578ad62
Shantell Moore <moores1@hawkmail.newpaltz.edu>
2019-03-13 10:56:18 -0400

    installed sequelize and sqlite3

--- Server/src/app.js
+++ Server/src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const{sequelize} = require('./models')
 
 const app = express()
 app.use(morgan('combined'))
